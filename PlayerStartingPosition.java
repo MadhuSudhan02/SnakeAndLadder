23084c5bfd2f8062dbe9b36cb5876eb60e3d53eb
Madhu Sudhan <m02sudhan@gmail.com>
2022-10-04 18:42:06 +0530

    player starting position

---
+++ PlayerStartingPosition.java
@@ -0,0 +1,6 @@
+public class PlayerStartingPosition {
+    public static void main(String[] args) {
+        int playerPosition = 0;
+        System.out.println("Position of player is " + playerPosition);
+    }
+}
